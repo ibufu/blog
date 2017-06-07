d2fd584931864880453f3490eb174c13140c230c
lingchenxuan <lingchenxuan@eluying.com>
2017-06-07 19:14:02 +0800

    修改build命令

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const shell = require("shelljs");
 const server = http.createServer();
 
 server.on('request', function(req, res) {
-    shell.exec('cd /root/blog && git pull origin master && hexo g');
+    shell.exec('cd /root/blog && git pull origin master && hexo clean && hexo g');
     res.end();
 });
 
