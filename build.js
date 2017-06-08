577def06baf0951357b8cd720451942f4d1e83dd
lingchenxuan <lingchenxuan@eluying.com>
2017-06-08 14:21:57 +0800

    更新命令

--- build.js
+++ build.js
@@ -4,13 +4,7 @@ const shell = require("shelljs");
 const server = http.createServer();
 
 server.on('request', function(req, res) {
-    shell.exec(`cd /root/blog
-        && git fetch
-        && git reset --hard origin/master
-        && git pull origin master
-        && npm i
-        && hexo clean
-        && hexo g`);
+    shell.exec('cd /root/blog && git fetch && git reset --hard origin/master && git pull origin master && npm i && hexo clean && hexo g');
     res.end('success');
     console.log(`${new Date().toString()}更新成功`)
 });
