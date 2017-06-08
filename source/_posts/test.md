d3e214d0fcb12d8df56fad571347e695958ad22a
lingchenxuan <lingchenxuan@eluying.com>
2017-06-08 10:52:37 +0800

    修改样式、加入百度统计、更新fancybox

--- source/_posts/test.md
+++ source/_posts/test.md
@@ -4,3 +4,5 @@ date: 2017-06-07 19:20:05
 tags:
 ---
 测试
+![](http://v1.qzone.cc/pic/201306/12/11/34/51b7ec46afea8156.jpg!600x600.jpg)
+![](http://v1.qzone.cc/pic/201306/12/11/34/51b7ec3803ae6844.jpg!600x600.jpg)
--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -94,7 +94,7 @@
 
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" data-fancybox="gallery"></a>');
     });
 
     $(this).find('.fancybox').each(function(){
